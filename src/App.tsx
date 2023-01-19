490854db2bbbdeae024c1a15b3027ef9d1499382
Demion-L <padovds@ukr.net>
2023-01-19 14:33:24 +0200

    actionCreators done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 import "./App.css";
+import { userSlice } from "./store/reducers/UserSlice";
+import { useAppDispatch, useAppSelector } from "./hooks/redux";
 
 function App() {
+  const { count } = useAppSelector((state) => state.userReducer);
+  const { increment } = userSlice.actions;
+  const dispatch = useAppDispatch();
+
   return (
     <div className='App'>
       <h1 className='bg-indigo-600 text-lg text-slate-50'>Hello world</h1>
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,24 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { IUser } from "../../models/IUser";
 
 export interface UserState {
   users: IUser[];
   isLoading: boolean;
   error: string;
-  count: number;
 }
 
 const initialState: UserState = {
   users: [],
   isLoading: false,
   error: "",
-  count: 0,
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    userFetching(state) {
+      state.isLoading = true;
+    },
+    userFetchingSucces(state, action: PayloadAction<IUser[]>) {
+      state.isLoading = false;
+      state.error = "";
+      state.users = action.payload;
+    },
+    userFetchingError(state, action: PayloadAction<string>) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
 });
 
 export const userReducer = userSlice.reducer;
