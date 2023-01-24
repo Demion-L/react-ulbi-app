9ae14797368e225db2f1f22cba08bf2c29603901
Demion-L <padovds@ukr.net>
2023-01-24 14:37:38 +0200

    RTKQuery implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,24 @@ import React from "react";
 import "./App.css";
 import { useAppDispatch, useAppSelector } from "./hooks/redux";
 import { fetchUsers } from "./store/reducers/ActionCreators";
+import { PostContainer } from "./components/PostContainer";
 
 function App() {
-  const dispatch = useAppDispatch();
-  const { users, isLoading, error } = useAppSelector(
-    (state) => state.userReducer
-  );
+  // const dispatch = useAppDispatch();
+  // const { users, isLoading, error } = useAppSelector(
+  //   (state) => state.userReducer
+  // );
 
-  React.useEffect(() => {
-    dispatch(fetchUsers());
-  }, []);
+  // React.useEffect(() => {
+  //   dispatch(fetchUsers());
+  // }, []);
 
   return (
     <div className='App'>
-      {isLoading && <h1>Loading...</h1>}
+      {/* {isLoading && <h1>Loading...</h1>}
       {error && <h1>Error: {error}</h1>}
-      <div>{JSON.stringify(users, null, 2)}</div>
+      <div>{JSON.stringify(users, null, 2)}</div> */}
+      <PostContainer />
     </div>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,18 @@
-import { combineReducers } from "@reduxjs/toolkit";
+import { combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { configureStore } from "@reduxjs/toolkit/dist/configureStore";
 import { userReducer } from "./reducers/UserSlice";
+import { postAPI } from "../services/PostService";
 
 const rootReducer = combineReducers({
   userReducer,
+  [postAPI.reducerPath]: postAPI.reducer,
 });
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(postAPI.middleware),
   });
 };
 
